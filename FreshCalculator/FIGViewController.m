62ef5fb55f969fd37364a7598853f950978769ee
John Figueiredo <john@johnfigueiredo.com>
2014-02-13 01:17:21 -0800

    Finishing up UI

--- FreshCalculator/FIGViewController.m
+++ FreshCalculator/FIGViewController.m
@@ -9,6 +9,7 @@
 #import "FIGViewController.h"
 
 @interface FIGViewController ()
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttonCollection;
 
 @end
 
@@ -17,13 +18,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [self customizeButtons];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Private Methods
+- (void)customizeButtons {
+  for (UILabel *label in self.buttonCollection) {
+//    CALayer * layer = [label layer];
+//    [layer setMasksToBounds:YES];
+//    [layer setCornerRadius:0.0]; //when radius is 0, the border is a rectangle
+//    [layer setBorderWidth:1.0];
+//    [layer setBorderColor:[[UIColor blackColor] CGColor]];
+  }
 }
 
 @end
